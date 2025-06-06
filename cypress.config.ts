a62fd148503457f8dc9e7a92797e19147980ec82
rajiv <rajiv.sapkota96@gmail.com>
2025-06-06 11:00:32 +0545

    updated retries config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 export default defineConfig({
   e2e: {
+    retries: {
+      runMode: 2,
+      openMode:2
+    },
     env: { ...process.env },
     baseUrl: process.env.BASEURL,
     //viewportWidth: 1660,
