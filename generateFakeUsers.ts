2f100cf2cbd389ec32b72de1ebfd6df122362dbd
rajiv <rajiv.sapkota96@gmail.com>
2025-06-09 14:46:00 +0545

    made some changes

--- generateFakeUsers.ts
+++ generateFakeUsers.ts
@@ -7,6 +7,4 @@ const user = {
   password: faker.internet.password({ length: 12 }),
 };
 
-writeFileSync("cypress/fixtures/loginData.json", JSON.stringify(user, null, 2));
 
-console.log("✅ Faker user written to cypress/fixtures/loginUser.json");
