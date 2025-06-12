62984b8f9b79461ca9e6b5322cbf36c132728ef8
rajiv <rajiv.sapkota96@gmail.com>
2025-06-12 15:47:58 +0545

    changed name in ascending order for sequential tests

---
+++ cypress/e2e/01LoginTests.cy.ts
@@ -0,0 +1,78 @@
+import { LoginPage } from "../pages/LoginPage";
+import { generateFakeuser } from "../fakerData/data";
+
+const user=generateFakeuser()
+const loginPage = new LoginPage();
+const username = Cypress.env("USER_NAME");
+const password = Cypress.env("PASSWORD");
+const url = Cypress.env("BASEURL")
+const invalidUsername = "test";
+const locked_out_user="locked_out_user"
+
+describe("Test cases for login", () => {
+  beforeEach( function()  {
+    cy.fixture("loginData").as("user");
+    loginPage.visitLoginPage();
+  });
+  it("TC-LOGIN-101:should login successfully with valid credentials", () => {
+    loginPage.typeUsername(username);
+    loginPage.typePassword(password);
+    loginPage.clickLoginButton();
+    loginPage.assertLoginSuccessful();
+  });
+
+  it("TC-LOGIN-102: test login with incorrect password", function() {
+    loginPage.typeUsername(username);
+    loginPage.typePassword(this.user.password);
+    loginPage.clickLoginButton();
+    loginPage.assertDisplayedText(
+      '[data-test="error"]',
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+    loginPage.assertUrl(url);
+  });
+
+  it("TC-LOGIN-103: login with empty username and password", () => {
+    loginPage.typeUsername("");
+    loginPage.typePassword("");
+    loginPage.clickLoginButton();
+    loginPage.assertDisplayedText(
+      '[data-test="error"]',
+      "Epic sadface: Username is required"
+    );
+    
+  });
+  
+  it("TC-LOGIN-104: login with invalid username and valid password", function () {
+    cy.log("typing invalid username");
+    loginPage.typeUsername(this.user.username);
+    loginPage.typePassword(password);
+    loginPage.clickLoginButton();
+    loginPage.assertDisplayedText(
+      '[data-test="error"]',
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+    loginPage.assertUrl(url);
+  });
+  
+  it("TC-LOGIN-105: password field is masked", () => {
+    loginPage.typePassword(password).should('have.attr', 'type', 'password')
+  });
+
+
+  it("TC-LOGIN-106:Test hide password button is available and working", () => {
+    loginPage.clickHidePasswordButton()
+  })
+
+  it("TC-LOGIN-107: restricted users are denied access", () => {
+    loginPage.typeUsername(locked_out_user)
+    loginPage.typePassword(password)
+    loginPage.clickLoginButton()
+    loginPage.assertDisplayedText(
+      '[data-test="error"]',
+      "Epic sadface: Sorry, this user has been locked out."
+    );
+  });
+
+
+});
---
+++ cypress/e2e/03Footer.cy.ts
@@ -0,0 +1,19 @@
+import { Footer } from "../pages/Footer"; 
+const footer = new Footer()
+
+describe("Tests for Footer", () => {
+    beforeEach(() => {
+        cy.login(Cypress.env('USER_NAME'), (Cypress.env('PASSWORD')))
+    })
+
+    it("TC-FOOTER-101:Test if Footer is visible", () => {
+        footer.assertSocialIconsVisible()
+    })
+
+    it("TC-FOOTER-103:Test if icons are clickable", () => {
+        footer.assertSocialIconsAreClickable()
+    })
+    it("TC-FOOTER-103:Test if Icons have Text", () => {
+      footer.assertTextLabelsOfIcons();
+    });
+})
